24da27a732eb3e55205a937001f21e6c9a60e9cf
Segev, Guy <guy.segev@news.com.au>
2019-10-15 00:38:11 +1100

    modifies notelist (might be subject to more changes

--- extension/src/injected/NoteList.tsx
+++ extension/src/injected/NoteList.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { TEXT_COLOR } from '../colorConstants';
+import { TEXT_COLOR, NOTE_COLOR } from '../colorConstants';
 import { Note } from './types';
 
 const StyledWrapper = styled.div`
     color: ${TEXT_COLOR};
+    background-color: ${NOTE_COLOR};
+    overflow: auto;
+    height: 230px;
+    padding: 3px;
+    font-size: 12px;
+`;
+
+const NoteWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 `;
 
 const formatTimestamp = (seconds: number): string => {
@@ -12,6 +23,38 @@ const formatTimestamp = (seconds: number): string => {
     date.setSeconds(seconds);
     const timeString = date.toISOString().substr(11, 8);
     return timeString;
+}
+
+class NoteComponent extends React.Component<{note: Note}, {hover: boolean}> {
+    constructor(props){
+        super(props);
+        this.state = {
+            hover: false,
+        }
+    }
+
+    onMouseEnter (e) {
+        this.setState({hover: true});
+    }
+    onMouseLeave (e) {
+        this.setState({hover : false});
+    }
+
+    render(){
+        const note = this.props.note;
+        return (
+            <NoteWrapper
+                key={note.note_id} 
+                onMouseEnter={this.onMouseEnter.bind(this)}
+                onMouseLeave={this.onMouseLeave.bind(this)}>
+                {this.state.hover
+                    ? "Edit Note"
+                    : formatTimestamp(note.timestamp)}
+                - {note.note}
+            </NoteWrapper> 
+        );
+
+    }
 }
 
 export default class NoteList extends React.Component<{notesList: Note[]}> {
@@ -23,9 +66,10 @@ export default class NoteList extends React.Component<{notesList: Note[]}> {
       const { notesList } = this.props
       return(
           <StyledWrapper>
-              {notesList.length ? notesList.map(n => (
-                <div key={n.note_id}>{formatTimestamp(n.timestamp)} - {n.note}</div>
-              )): "There are no notes for this video" }
+                { notesList.length 
+                    ? notesList.map(n => (<NoteComponent note={n}/>))
+                    : "There are no notes for this video"
+                }
           </StyledWrapper>
       );
   }
