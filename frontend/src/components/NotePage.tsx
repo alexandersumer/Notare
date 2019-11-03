38a00a45680d4156c8cb95353f2c78d94b2e9a37
Segev, Guy <guy.segev@news.com.au>
2019-11-03 15:27:45 +1100

    fixes naming conventiosn

---
+++ frontend/src/components/NotePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+
+};
+
+class NotePage extends React.Component<Props> {
+  constructor(props: Props){
+      super(props);
+  }
+
+  render(){
+    return (<div>Testing</div>)
+  }
+}
+
+export default NotePage;
