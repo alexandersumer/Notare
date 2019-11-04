89b082324c8ad6b0eb9a5420d63e01391663d0db
Alexander Jones <alexanderj2357@gmail.com>
2019-11-04 18:10:18 +1100

    created backend for notes and videos

--- frontend/src/types.tsx
+++ frontend/src/types.tsx
@@ -6,3 +6,12 @@ export type NoteType = {
   timestamp: number,
   video_title: string,
 }
+
+export type VideoType = {
+  video_id: string,
+  user_id: number,
+  video_title: string,
+  categories: string,
+  notes_ids: [number],
+  notes_count: number,
+}
