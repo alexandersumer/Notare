d0a4cdb17c83bf7f27ea6a0558a6cb68f4fb9c30
Mitchell <mitchell_shelton@y7mail.com>
2019-11-04 13:31:08 +1100

    videos are listed on the frontend with their thumbnails

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import HomePage from "./HomePage";
 import AboutUsPage from "./AboutUsPage";
 import NotePage from "./NotePage";
+import VideoPage from "./VideoPage";
+
 
 class App extends React.Component {
   render() {
@@ -12,11 +14,13 @@ class App extends React.Component {
           <nav>
             <Link to="/">Home</Link>
             <Link to="/Notes">Notes</Link>
+            <Link to="/Videos">Videos</Link>
             <Link to="/AboutUs">About us</Link>
           </nav>
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/Notes" component={NotePage} />
+            <Route exact path="/Videos" component={VideoPage} />
             <Route exact path="/AboutUs" component={AboutUsPage} />
           </Switch>
         </div>
