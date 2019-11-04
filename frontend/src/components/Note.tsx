a6b7791803b847a3d5f2b31b6df5e16dfc095c19
Segev, Guy <guy.segev@news.com.au>
2019-11-04 19:21:24 +1100

    adds link to youtube video from both the thumbnail and the timestamp

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -4,6 +4,7 @@ import Thumbnail from "./Thumbnail";
 import { NoteType } from "../types";
 import { styled as materialStyled } from "@material-ui/core/styles";
 import { PINK_COLOR } from "../colorConstants";
+import Link from '@material-ui/core/Link';
 
 const formatTimestamp = (seconds: number): string => {
   let date = new Date(0);
@@ -11,6 +12,9 @@ const formatTimestamp = (seconds: number): string => {
   if (seconds >= 60) return date.toISOString().substr(11, 8);
   return date.toISOString().substr(14, 5);
 };
+
+const getYoutubeTimeLink = (video_id: string, timestamp: number) : string =>
+  (`https://youtu.be/${video_id}?t=${Math.floor(timestamp)}`)
 
 const NoteStyle = materialStyled(Box)({
   height: "80px",
@@ -24,12 +28,16 @@ interface Props {
 
 const Note = (props: Props) => {
   const { noteData } = props;
+  const YoutubeLink = (props: any) => (
+    <Link href={getYoutubeTimeLink(noteData.video_id, noteData.timestamp)}>{props.children}</Link>
+  );
+
   return (
     <Box display="flex" flexDirection="row" m={3} flexGrow={1}>
-      <Thumbnail video_id={noteData.video_id}></Thumbnail>
+    <YoutubeLink><Thumbnail video_id={noteData.video_id}></Thumbnail></YoutubeLink>
       <NoteStyle ml display="flex" flexDirection="row" flexGrow={1}>
         <Box p={1} mr={1} display="flex" alignItems="center">
-          <Box>{formatTimestamp(noteData.timestamp)}</Box>
+          <YoutubeLink>{formatTimestamp(noteData.timestamp)}</YoutubeLink>
         </Box>
         <Box p={1} display="flex" alignItems="center">
           {noteData.note}
