4d0a9ad98fcfa8b125013e78f5c9b9ab8bd78ea0
Alexander Jones <alexanderj2357@gmail.com>
2019-11-21 12:17:06 +1100

    checkpoint

--- backend/tests/notare/test_backend.py
+++ backend/tests/notare/test_backend.py
@@ -2,44 +2,96 @@ import pytest
 import requests
 from flask import json
 
-# log into my account
-def test_post_login_account():
 
+def get_auth_data():
     body = {"email": "a@b.com", "password": "password"}
-    print(f"Logging in with details: {body}")
     response = requests.post("http://127.0.0.1:5000/v1/login", json=body)
-    print(response.status_code)
-    print(response.text)
+    response_data = json.loads(response.text)
+    return response_data
+
+
+def auth_logout():
+    auth_data = get_auth_data()
+
+    requests.delete(
+        "http://127.0.0.1:5000/v1/logout",
+        headers={"Authorization": f"Bearer {auth_data['accessToken']}"},
+    )
+
+
+def test_delete_logout():
+    auth_data = get_auth_data()
+
+    response = requests.delete(
+        "http://127.0.0.1:5000/v1/logout",
+        headers={"Authorization": f"Bearer {auth_data['accessToken']}"},
+    )
 
     assert response.status_code == 200
-    r = json.loads(response.text)
-    assert "user_id" in r
-    assert "accessToken" in r
-    return r
+    response_data = json.loads(response.text)
+    assert "message" in response_data
 
 
-# get notes for myself
+def test_post_login():
+    body = {"email": "a@b.com", "password": "password"}
+    response = requests.post("http://127.0.0.1:5000/v1/login", json=body)
+
+    assert response.status_code == 200
+    response_data = json.loads(response.text)
+    assert "user_id" in response_data
+    assert "accessToken" in response_data
+
+
+def test_post_create_account():
+    body = {"email": "c@d.com", "password": "password"}
+    response = requests.post("http://127.0.0.1:5000/v1/createAccount", json=body)
+
+    assert response.status_code == 200
+    response_data = json.loads(response.text)
+    assert "user_id" in response_data
+    assert "accessToken" in response_data
+
+
 def test_get_notes():
+    auth_data = get_auth_data()
 
-    # need to login to get my accessToken
-    me = test_post_login_account()
-
-    print(f"my details {me}")
-    params = {"user_id": me["user_id"]}
+    params = {"user_id": auth_data["user_id"]}
     response = requests.get(
         "http://127.0.0.1:5000/v1/notes",
-        headers={"Authorization": f"Bearer {me['accessToken']}"},
+        headers={"Authorization": f"Bearer {auth_data['accessToken']}"},
         params=params,
     )
-    print(response.status_code)
-    print(response.text)
 
     assert response.status_code == 200
-    r = json.loads(response.text)
-    assert "notes" in r
-    assert "num_notes" in r
-    assert len(r["notes"]) == r["num_notes"]
-    for note in r["notes"]:
+    response_data = json.loads(response.text)
+    assert "notes" in response_data
+    assert "num_notes" in response_data
+    assert len(response_data["notes"]) == response_data["num_notes"]
+    for note in response_data["notes"]:
+        assert "note_id" in note
+        assert "note" in note
+        assert "user_id" in note
+        assert "timestamp" in note
+        assert "time_created" in note
+        assert "last_edited" in note
+
+
+def test_get_notes():
+    auth_data = get_auth_data()
+
+    params = {"user_id": auth_data["user_id"]}
+    response = requests.get(
+        "http://127.0.0.1:5000/v1/notes",
+        headers={"Authorization": f"Bearer {auth_data['accessToken']}"},
+        params=params,
+    )
+
+    assert response.status_code == 200
+    response_data = json.loads(response.text)
+    assert "notes" in response_data
+    assert "num_notes" in response_data
+    assert len(response_data["notes"]) == response_data["num_notes"]
+    for note in response_data["notes"]:
         assert "note_id" in note
         assert "note" in note
         assert "user_id" in note
