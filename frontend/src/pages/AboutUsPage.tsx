140647af653192408be7a5e1d6e0b26af7fc8a39
Daniel Brockwell <brockwell.daniel@gmail.com>
2019-11-23 18:16:35 +1100

    Added demo photo to home page, changed font styling/padding, moved subtitle to About Us page

--- frontend/src/pages/AboutUsPage.tsx
+++ frontend/src/pages/AboutUsPage.tsx
@@ -102,6 +102,8 @@ class AboutUsPage extends React.Component<Props> {
           <Box mt={3} display="flex" flexDirection="column" alignItems="center">
             <Box p={2} />
             <Typography variant="h2">Who the heck are these guys?</Typography>
+            <Box p={1} />
+            <Typography variant="h6">WARNING: Notare may contain blood, sweat and tears from the following individuals:</Typography>
             <Box p={2} />
             <Box display="flex" flexDirection="row">
               {profileInfo.map(Profile)}
