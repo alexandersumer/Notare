4ec72e58d57f4727a3e75c544eb524ca37b1daf6
Daniel Brockwell <brockwell.daniel@gmail.com>
2019-11-23 13:50:30 +1100

    added photos

--- frontend/src/pages/AboutUsPage.tsx
+++ frontend/src/pages/AboutUsPage.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+// import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import Link from "@material-ui/core/Link";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import Container from "../components/Container";
 import Typography from "@material-ui/core/Typography";
+import Daniel from "../daniel.png";
+import Alexander from "../alexander.png";
+import Guy from "../guy.png";
+import Mitchell from "../mitchell.png";
 
 const Profile = (profile: ProfileType) => {
   return (
     <Box m={3} display="flex" flexDirection="column" alignItems="center">
       <Box>
-        <AccountCircleIcon />
+        <img width="200px" height="200px" src={profile.image}/>
       </Box>
       <Box p={1} />
       <Typography variant="h5" color="textPrimary">
@@ -44,6 +48,7 @@ const Profile = (profile: ProfileType) => {
 };
 
 type ProfileType = {
+  image: any;
   name: string;
   description: string;
   gitLink: string;
@@ -52,24 +57,28 @@ type ProfileType = {
 
 const profileInfo = [
   {
+    image: Alexander,
     name: "Alexander Jones",
     description: "Fullstack Developer",
     gitLink: "https://github.com/alexanderj2357",
     liLink: "https://www.linkedin.com/in/alexanderj2357/"
   },
   {
+    image: Daniel,
     name: "Daniel Brockwell",
-    description: "UX Designer & Frontend Developer",
+    description: "Frontend Developer & UX",
     gitLink: "https://github.com/Actom360",
     liLink: "https://www.linkedin.com/in/danielbrockwell/"
   },
   {
+    image: Guy,
     name: "Guy Segev",
-    description: "Frontend Developer",
+    description: "Frontend Developer & UX",
     gitLink: "https://github.com/atiredturtle",
     liLink: "https://www.linkedin.com/in/guy-segev-98a27110a/"
   },
   {
+    image: Mitchell,
     name: "Mitchell Shelton",
     description: "Fullstack Developer",
     gitLink: "https://github.com/armoured",
