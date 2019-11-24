4ef9ca2ef939a8892b63587f4970624f30422b13
Alexander Jones <alexanderj2357@gmail.com>
2019-11-24 15:50:06 +1100

    small UI fixes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./pages/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+require("./index.css");
+
 ReactDOM.render(<App />, document.getElementById("root"));
